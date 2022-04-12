6234d8c664c7240dfd1f6c6d9c7ed6150e567597
Andreas Säuberli <andreas.saeuberli@gmail.com>
2022-04-12 17:07:42 +0200

    add undersampling as argument in CLI

--- src/data.py
+++ src/data.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 from datetime import datetime
-import dateutil.parser
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Sequence
 from xml.etree import ElementTree as ET
+
+import dateutil.parser
+
+from log import logger
 
 
 @dataclass
@@ -39,3 +42,39 @@ def parse_subject(filename: str) -> Subject:
     label = Path(filename).parent.name
     assert label in ["neg", "pos"]
     return Subject(subject_id, posts, label == "pos")
+
+
+def undersample_subjects(subjects: Sequence[Subject], ratio: float) -> List[Subject]:
+    """Undersample negative subjects to achieve the given negative-to-positive ratio."""
+    num_pos_subjects = num_neg_subjects = 0
+    for subject in subjects:
+        if subject.label:
+            num_pos_subjects += 1
+        else:
+            num_neg_subjects += 1
+    neg_ratio_to_delete = 1 - ratio * num_pos_subjects / num_neg_subjects
+    undersampled_subjects = []
+    i = 0
+    for subject in subjects:
+        if subject.label:
+            # Keep all positive subjects
+            undersampled_subjects.append(subject)
+        else:
+            i += neg_ratio_to_delete
+            if i >= 1:
+                # Delete some negative subjects according to ratio
+                i -= 1
+            else:
+                # Keep remaining negative subjects
+                undersampled_subjects.append(subject)
+
+    num_neg_subjects_after = 0
+    for subject in undersampled_subjects:
+        if not subject.label:
+            num_neg_subjects_after += 1
+    logger.info(
+        f"Undersampled {num_neg_subjects} to {num_neg_subjects_after} "
+        "negative subjects."
+    )
+
+    return undersampled_subjects
